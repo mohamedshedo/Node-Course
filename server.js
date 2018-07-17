7aaae5af3dbefbd1e0979194ae3f8bbcbec441c9
MohamedShedo <mohamedshedo@gmail.com>
2018-07-17 22:29:13 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let app=express();
+let hbs = require('hbs');
+app.use(express.static(__dirname+'/public'));
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use('/',function(req,res,next){
+    res.send('here');
+   // next();
+});
+
+app.get('/',(req,res)=>{
+    
+    res.send({name:'hugh',age:13});
+});
+
+app.get('/home',function(req,res){
+    res.render('home.hbs',{pageTitle:'Home'});
+});
+
+app.get('/about',(req,res)=>{
+
+    res.render('about.hbs',{pageTitle:'About Page',
+    currentYear:2018
+});
+});
+
+app.get('/bad',(req,res)=>{
+    res.send();
+});
+
+app.listen(3000);
